17fe2a8540f2865de95bfcbce8119469b708a981
cesar <cesar.caju@academico.ifpb.edu.br>
2024-09-25 13:30:07 -0300

    estilização base do site e tela home criados e input de pesquisa funcionando

--- index.js
+++ index.js
@@ -13,12 +13,48 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get('/home', function(req, res){
-    res.render('series')
+
+function gerarListaFor(t_inicial, t_final, parametro, incluso, obj){
+    for (let i = t_inicial; i >= t_final; i--) {
+        if (!parametro || api[i][parametro].toLowerCase().includes(incluso.toLowerCase())) {
+            if (Object.keys(obj).length < 12) {
+                obj[Object.keys(obj).length] = api[i];
+            }
+        }
+    }
+}
+
+function gerarListaWhile(genero, obj) {
+    let i = api.length - 1;
+    while (i >= 0 && Object.keys(obj).length < 12) {
+        if (!genero || api[i].generos.includes(genero)) {
+            obj[Object.keys(obj).length] = api[i];
+        }
+        i--;
+    }
+}
+
+app.get('/', async function(req, res) {
+    let lancamentos = {};
+    gerarListaFor(api.length - 1, api.length - 13, null, null, lancamentos);
+
+    let acao = {};
+    gerarListaWhile("Action", acao);
+
+    let comedias = {};
+    gerarListaWhile("Comedy", comedias);
+
+    let dramas = {};
+    gerarListaWhile("Drama", dramas);
+
+    res.render('series', {lancamentos, acao, comedias, dramas})
 })
 
 app.get('/series', function(req, res){
-    res.send(`${req.body.titulo}`)
+    let nomePesquisado = req.query.titulo
+    let data = {};
+    gerarListaFor(api.length - 1, 0, 'titulo', nomePesquisado, data);
+    res.render('search', {data})
 })
 
 app.listen('3000', function(){
